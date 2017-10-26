9e38f1185e4e5eabd40cb70e94f53f08abe898a2
Chris Lanus - work <cqlanus@gmail.com>
2017-10-26 14:30:17 -0500

    formatting more daily station dataframes

--- src/create_daily_stations_df.py
+++ src/create_daily_stations_df.py
@@ -14,6 +14,14 @@ def format_df(file, col_name):
 def daily_stations_df():
     max_temps = format_df('../data/dly-tmax-normal.csv', 'max_temps')
     min_temps = format_df('../data/dly-tmin-normal.csv', 'min_temps')
-    print(min_temps[100:120])
+    gdd_40 = format_df('../data/dly-grdd-base40.csv', 'gdd_40')
+    gdd_50 = format_df('../data/dly-grdd-base50.csv', 'gdd_50')
+    mtd_precip = format_df('../data/mtd-prcp-normal.csv', 'mtd_precip')
+    mtd_snow = format_df('../data/mtd-snow-normal.csv', 'mtd_snow')
+    ytd_precip = format_df('../data/ytd-prcp-normal.csv', 'ytd_precip')
+    ytd_snow = format_df('../data/ytd-snow-normal.csv', 'ytd_snow')
+    dly_precip_50 = format_df('../data/dly-prcp-50pctl.csv', 'dly_precip_50')
+    dly_precip_75 = format_df('../data/dly-prcp-75pctl.csv', 'dly_precip_75')
+    print(mtd_snow[100:120])
 
 daily_stations_df()
